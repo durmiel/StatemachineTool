c820b8670d8d6aa4713305234a1193f463629a48
Norbert Neumann <norbert.neumann11@gmail.com>
2019-06-18 21:36:54 +0200

    implement load file dialog and save file dialog, add prototypes for adding states and transitions

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import tkinter.filedialog as fdialog
 
 class Gui(object):
     """Gui is the root element of the Statemachine Tool. It
@@ -11,37 +11,68 @@ class Gui(object):
         self.__uiRoot.title(title)
         self.__uiRoot.geometry('612x341')
 
-        BtLoadFile = tk.Button(self.__uiRoot, text="Load", width=10)
-        BtLoadFile.grid(row=1, column=1, padx=7.5, pady=6)
+        self.__initFrameButtons(self.__uiRoot)
 
-        BtSaveFile = tk.Button(self.__uiRoot, text="Save", width=10)
-        BtSaveFile.grid(row=1, column=2, padx=7.5, pady=6)
-
-        BtNewState = tk.Button(self.__uiRoot, text="State", width=10)
-        BtNewState.grid(row=1, column=3, padx=7.5, pady=6)
-
-        BtNewTransition = tk.Button(self.__uiRoot, text="Transistion",
-                                    width=10)
-        BtNewTransition.grid(row=1, column=4, padx=7.5, pady=6)
-
-        BtExit = tk.Button(self.__uiRoot, text="Exit", width=10,
-                           command=self.__exitGui)
-        BtExit.grid(row=1, column=5, padx=7.5, pady=6)
-
-        FrameStatemachine = tk.Frame(self.__uiRoot, bg='cyan', width=600,
-                                     height=300)
-        FrameStatemachine.grid(row=2, column=1, columnspan=5)
+        FrameStatemachine = tk.Frame(self.__uiRoot, width=600, height=300,
+                                     highlightbackground="black", highlightcolor="black",
+                                     highlightthickness=1)
+        FrameStatemachine.pack()
 
         self.__uiRoot.mainloop()
+        pass
+
+    def __initFrameButtons(self, rootUi):
+        FrameButtons = tk.Frame(self.__uiRoot, width=600, height=100)
+        FrameButtons.pack()
+
+        BtLoadFile = tk.Button(FrameButtons, text="Load",
+                               width=10, command=self.__loadStatemachineFromFile)
+        BtLoadFile.grid(row=1, column=1, padx=7.5, pady=6)
+
+        BtSaveFile = tk.Button(FrameButtons, text="Save",
+                               width=10, command=self.__saveStatemachineFromFile)
+        BtSaveFile.grid(row=1, column=2, padx=7.5, pady=6)
+
+        BtNewState = tk.Button(FrameButtons, text="State",
+                               width=10, command=self.__addState)
+        BtNewState.grid(row=1, column=3, padx=7.5, pady=6)
+
+        BtNewTransition = tk.Button(
+            FrameButtons, text="Transistion", width=10, command=self.__addTransition)
+        BtNewTransition.grid(row=1, column=4, padx=7.5, pady=6)
+
+        BtExit = tk.Button(FrameButtons, text="Exit", width=10,
+                           command=self.__exitGui)
+        BtExit.grid(row=1, column=5, padx=7.5, pady=6)
         pass
 
     def __exitGui(self):
         self.__uiRoot.destroy()
         pass
 
+    def __loadStatemachineFromFile(self):
+        fileName = fdialog.askopenfilename(
+            parent=self.__uiRoot, initialdir='C:/',
+            title='Choose file',
+            filetypes=[('Text Files', '.txt'),
+                       ('XML Files', '.xml')]
+            )
+        if fileName is "":
+            return
+        print(fileName)
+        pass
 
-class GuiState(tk.Frame):
-    """Gui element for the state class"""
+    def __saveStatemachineFromFile(self):
+        fileName = fdialog.asksaveasfilename(
+            parent=self.__uiRoot, initialdir='C:/',
+            title='Choose file',
+            filetypes=[('Text Files', '.txt'),
+                       ('XML Files', '.xml')]
+            )
+        pass
 
-    def __init__(self, parent, stateName):
-        tk.Frame.__init__(self, parent)
+    def __addState(self):
+        pass
+
+    def __addTransition(self):
+        pass
